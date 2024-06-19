f0c0a9ee329ee16fd7f963d20983e37b97ae5c45
Pedro Henrique Costa Dias <44750670+RollerScouter@users.noreply.github.com>
2024-06-19 15:44:36 -0300

    Add files via upload

---
+++ src/main/java/br/edu/up/CinemaManager/view/TransacaoView.java
@@ -0,0 +1,37 @@
+package br.edu.up.CinemaManager.view;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+public class TransacaoView {
+    private static final Logger logger = LogManager.getLogger(TransacaoView.class);
+    static Scanner scannerTransacao = new Scanner(System.in);
+
+    public static void menuTransacao(){
+        Integer opcaoTransacao = 1;
+        while (opcaoTransacao != 5){
+            System.out.println("//////////////TRANSAÇÕES//////////////");
+            Integer opTransacao;
+            System.out.println("1-Vender Ingresso");
+            System.out.println("2-Valor Arrecadado");
+            System.out.println("3-Listar Transações");
+            System.out.println("4-Voltar");
+            opcaoTransacao = scannerTransacao.nextInt();
+            switch(opcaoTransacao){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+}
+
