271a3450237f265afed4ec2f25da0c216d862fa4
Gustavo Tavares Espenchitt <gustavo.t.espenchitt@gmail.com>
2024-06-19 22:19:47 -0300

    Mudanças no FilmeView

--- src/main/java/br/edu/up/CinemaManager/view/FilmeView.java
+++ src/main/java/br/edu/up/CinemaManager/view/FilmeView.java
@@ -1,5 +1,7 @@
 package br.edu.up.CinemaManager.view;
 
+import br.edu.up.CinemaManager.controllers.FilmeController;
+import br.edu.up.CinemaManager.models.Filme;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,6 +10,7 @@ import java.util.Scanner;
 public class FilmeView {
     private static final Logger logger = LogManager.getLogger(FilmeView.class);
     static Scanner scannerFilme = new Scanner(System.in);
+    private static FilmeController filmeController = new FilmeController();
 
     public static void menuFilme() {
         Integer opcaoFilme = 1;
@@ -21,8 +24,10 @@ public class FilmeView {
             opcaoFilme = scannerFilme.nextInt();
             switch (opcaoFilme) {
                 case 1:
+                    adicionarFilme();
                     break;
                 case 2:
+                    removerFilme();
                     break;
                 case 3:
                     break;
@@ -32,5 +37,27 @@ public class FilmeView {
                     break;
             }
         }
+    }
+
+    public static void adicionarFilme() {
+        System.out.println("Informe o título do filme:");
+        String titulo = scannerFilme.nextLine(); // Consumir quebra de linha pendente
+        titulo = scannerFilme.nextLine(); // Ler o título corretamente
+        System.out.println("Informe o gênero do filme:");
+        String genero = scannerFilme.next();
+        System.out.println("Informe o diretor do filme:");
+        String diretor = scannerFilme.nextLine(); // Consumir quebra de linha pendente
+        diretor = scannerFilme.nextLine(); // Ler o título corretamente
+        System.out.println("Informe a idade indicativa do filme:");
+        Integer idade = scannerFilme.nextInt();
+        Filme filme = new Filme(titulo, genero, diretor, idade);
+        filmeController.create(filme);
+    }
+
+    public static void removerFilme() {
+        System.out.println("Informe o título do filme a ser removido:");
+        String tituloRemover = scannerFilme.nextLine();// Consumir quebra de linha pendente
+        tituloRemover = scannerFilme.nextLine();
+        Filme filmeRemover = buscarFilme(tituloRemover);
     }
 }
