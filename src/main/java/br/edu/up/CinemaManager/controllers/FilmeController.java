3c91e15bb3f50d48ad681a68e44c7ef24798328b
Pedro Henrique Costa Dias <44750670+RollerScouter@users.noreply.github.com>
2024-06-26 19:33:44 -0300

    Update FilmeController.java

--- src/main/java/br/edu/up/CinemaManager/controllers/FilmeController.java
+++ src/main/java/br/edu/up/CinemaManager/controllers/FilmeController.java
@@ -1,1 +1,80 @@
+package br.edu.up.CinemaManager.controllers;
 
+import br.edu.up.CinemaManager.daos.FilmeDao;
+import br.edu.up.CinemaManager.daos.GenericDao;
+import br.edu.up.CinemaManager.exceptions.FilmeNotFoundException;
+import br.edu.up.CinemaManager.models.Filme;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class FilmeController extends AbstractCRUD<Filme> {
+    private static final Logger logger = LogManager.getLogger(FilmeController.class);
+    private GenericDao<Filme> filmeDao;
+
+    public FilmeController() {
+        filmeDao = new FilmeDao();
+        items = filmeDao.carregar();
+    }
+
+    public void adicionarFilme(Filme filme) {
+      \**
+      *Método para adicionar filmes
+      * @return
+      * @param filmeDao
+      * @param logger
+      *\  
+        for (Filme i : items) {
+            if (i.getTitulo().equals(filme.getTitulo())) {
+                logger.warn("Tentativa de adicionar um filme já existente: " + filme.getTitulo());
+                return;
+            }
+        }
+        create(filme);
+        filmeDao.salvar(items); // Salva a lista atualizada de filmes
+        logger.info("Filme adicionado: " + filme.getTitulo());
+    }
+
+    public void deletarFilme(Integer id) {
+        Filme filme = buscarFilmeId(id);
+        if (filme != null) {
+            delete(filme);
+            filmeDao.salvar(items); // Salva a lista atualizada de filmes
+            logger.info("Filme removido: " + filme.getTitulo() + ", ID" + filme.getId());
+        } else {
+            logger.warn("Tentativa de remover um filme não encontrado: " + filme.getTitulo() + ", ID: " + filme.getId());
+        }
+    }
+
+    public Filme buscarFilmeTitulo(String titulo) {
+        try {
+            for (Filme i : items) {
+                if (i.getTitulo().equals(titulo)) {
+                    return i;
+                }
+            }
+        }catch (FilmeNotFoundException e){
+            logger.warn("Filme não encontrado: " + e);
+        }
+        return null;
+    }
+
+    public Filme buscarFilmeId(Integer id) {
+        for (Filme i : items) {
+            if (i.getId() == id) {
+                return i;
+            }
+        }
+        logger.warn("Filme não encontrado: " + id);
+        return null;
+    }
+
+    public List<Filme> listarFilmesOrdenadosPorTitulo() {
+        List<Filme> filmesOrdenados = new ArrayList<>(items);
+        filmesOrdenados.sort(Comparator.comparing(Filme::getTitulo));
+        return filmesOrdenados;
+    }
+}
